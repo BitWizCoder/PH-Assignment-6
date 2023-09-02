7adc1e6d6e27742a3afd31740d6da7aeb1dbbad6
mhmdNoman <mhmdnoman01@gmail.com>
2023-09-02 18:16:38 +0600

    fix css

--- script.js
+++ script.js
@@ -1,68 +1,84 @@
-// Select Elements
-const categoryId = document.getElementById("categorie");
+// Elements
+const categoryContainer = document.getElementById("category");
 const cardContainer = document.getElementById("card-container");
 
-// Categorie buttons and return categorie id
-async function getId(id) {
-  try {
-    let res = await fetch(
-      "https://openapi.programming-hero.com/api/videos/categories"
+async function getData() {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/videos/categories"
+  );
+  const data = await res.json();
+
+  // Handle button clicks
+  async function handleBtn(id) {
+    cardContainer.innerHTML = "";
+    const res = await fetch(
+      `https://openapi.programming-hero.com/api/videos/category/${id}`
     );
-    let data = await res.json();
+    const data = await res.json();
+    data.data.forEach((content) => {
+      console.log(content);
+      const div = document.createElement("div");
+      div.classList.add(
+        "max-w-[312px]",
+        "max-h[315px]",
+        "border",
+        "border-sky-200"
+      );
+      div.innerHTML = `
+      <div class="relative w-[300px]">
+      <img src="${content.thumbnail}" class='h-[200px] w-full' />
+      <p class="absolute right-0 top-36 text-white bg-slate-900 p-1 rounded-md">${
+        content.others.posted_date
+      }</p>
+    </div>
+    <div class="flex items-center gap-2 mt-5">
+      <img src="${
+        content.authors[0].profile_picture
+      }" alt="avatar" class="w-10 h-10 rounded-full" />
+      <p class="font-semibold text-base">
+        ${content.title}
+      </p>
+    </div>
+    <p class="mt-3 text-sm">
+      ${content.authors[0].profile_name}
+      ${
+        content.authors[0].verified
+          ? '<img src="./img/blue-tick.svg" alt="" class="inline-block" />'
+          : ""
+      }
+    </p>
+    <p class="mt-1 text-sm">${content.others.views} views</p>
+      `;
 
-    for (let i = 0; i < data.data.length; i++) {
-      const category = document.createElement("button");
-      category.classList.add("btn");
+      cardContainer.appendChild(div);
+    });
+  }
+  // handleBtn(1000);
 
-      // Set the button text to the category name
-      category.innerText = data.data[i].category;
-      let catId = data.data[i].category_id;
-      // Add an event listener to each button to perform some action (e.g., show category-specific content)
-      category.addEventListener("click", () => {
-        // console.log(data.data[i].category_id);
-        async function displayContent(id) {
-          try {
-            let res = await fetch(
-              `https://openapi.programming-hero.com/api/videos/category/${id}`
-            );
-            let innerData = await res.json();
-            console.log(innerData.data[0]);
-            const card = document.createElement("div");
-            card.classList.add("max-w-xs");
+  // Loop through the categories and create buttons
+  data.data.forEach((category) => {
+    const div = document.createElement("div");
+    const categoryName = category.category;
 
-            const cardHtml = `
-            <div>
-            <img src="./img/thumbnail.png" alt="video thumbnail" />
-          </div>
-          <div class="flex items-center gap-2 mt-5">
-            <img src="./img/avatar.png" alt="avatar" />
-            <p class="font-semibold text-base">
-              Building a Winning UX Strategy Using the Kano Model
-            </p>
-          </div>
-          <p class="mt-3 text-sm">
-            Awlad Hossain
-            <img src="./img/blue-tick.svg" alt="" class="inline-block" />
-          </p>
-          <p class="mt-1 text-sm">91K views</p>
-            
-            `;
+    // Create a button with an event listener
+    const button = document.createElement("button");
+    button.setAttribute("id", "tab-btn");
+    button.classList.add("btn")
 
-            card.innerHTML = cardHtml;
-
-            cardContainer.appendChild(card);
-          } catch (error) {
-            console.log("Error fetching data:", error);
-          }
-        }
-        displayContent(catId);
+    button.textContent = categoryName;
+    button.addEventListener("click", function () {
+      const buttons = document.querySelectorAll("#tab-btn");
+      buttons.forEach((btn) => {
+        btn.classList.remove("bg-red-500");
       });
 
-      categoryId.appendChild(category);
-    }
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }
+      button.classList.add("bg-red-500");
+      handleBtn(category.category_id);
+    });
+
+    div.appendChild(button);
+    categoryContainer.appendChild(div);
+  });
 }
 
-getId();
+getData();
